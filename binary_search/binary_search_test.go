fdefe08dfd77ac8919f1b844f0974c3988cf1de6
Andrey Golikov <aagolik8@mts.ru>
2025-03-02 21:30:01 +0300

    add binary search

---
+++ binary_search/binary_search_test.go
@@ -0,0 +1,27 @@
+package binary_search
+
+import "testing"
+
+func Test_search(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: testing.CoverMode(), args: struct {
+			nums   []int
+			target int
+		}{nums: []int{-1, 0, 3, 5, 9, 12}, target: 9}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := search(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("search() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

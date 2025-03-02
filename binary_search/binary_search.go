24d901d4e626dd05d02a311d76151778231d1958
Andrey Golikov <aagolik8@mts.ru>
2025-03-02 22:26:11 +0300

    add searchMatrix

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,9 +1,27 @@
 package binary_search
 
+func searchMatrix(matrix [][]int, target int) bool {
+	left, right := 0, len(matrix)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if target >= matrix[mid][0] && target <= matrix[mid][len(matrix[mid])-1] {
+			if res := search(matrix[mid], target); res != -1 {
+				return true
+			}
+			return false
+		} else if target > matrix[mid][0] {
+			left = mid + 1
+		} else if target < matrix[mid][len(matrix[mid])-1] {
+			right = mid - 1
+		}
+	}
+	return false
+}
+
 func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
 	for left <= right {
-		mid := left + (right - left/2)
+		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
 		} else if nums[mid] < target {
